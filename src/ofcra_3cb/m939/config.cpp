d232a93fd6babb20693d812c36ab52766ddd5f0d
alanriddell <a.riddell@daxtra.com>
2025-08-12 22:24:47 +0100

    Create config.cpp
    less explodey version of M939

---
+++ src/ofcra_3cb/m939/config.cpp
@@ -0,0 +1,59 @@
+class CfgPatches
+{
+	class ofcra_3cb_m939
+	{
+		author = "OFCRA Wombat";
+		skipWhenMissingDependencies=1;
+		units[]=
+		{
+
+		};
+
+		requiredAddons[]=
+		{
+			"rhs_main_loadorder",
+			"UK3CB_Factions_Vehicles_Tseries"
+		};
+	};
+};
+
+class HitPoints;
+
+
+class CfgVehicles
+{
+	class UK3CB_B_M939_Guntruck_G;
+	class UK3CB_CW_US_B_EARLY_M939_Guntruck : UK3CB_B_M939_Guntruck_G {
+		class HitPoints : HitPoints{
+			class HitEngine;
+			class HitBody;
+			class HitFuel;
+			class HitHull;
+		}
+	};
+	class OFCRA_HARDENED_M939_EARLY: UK3CB_CW_US_B_EARLY_M939_Guntruck
+	{
+		armor=32; //from armor = 32;  
+		armorStructural=8; //from armorStructural = 4; higher = less damage.  How much of the damage from parts goes through to object
+		class HitPoints : HitPoints // inherit ALL from UK3CB_CW_US_B_EARLY_M939_Guntruck
+		{
+			class HitEngine : HitEngine // inherit HitEngine from UK3CB_CW_US_B_EARLY_M939_Guntruck
+			{
+				radius = 1; // from radius = 0.45; Make easier to KO engine
+				armor = 27; // from armor = 3; //multiplier from object armor??, this is HP
+				passThrough = 0.5; // Damage to engine does not damage the vehicle
+			};
+			class HitBody : HitBody {
+				passThrough = 1;	
+				armor = 0.35;
+				radius = 0.1; //make smaller 
+			};
+			class HitFuel : HitFuel {
+				passThrough = 0.1;
+			}
+			class HitHull : HitHull {
+				armor = 100;
+			}
+		};
+	};			
+};
